b5639daebb9864ce0b866cd0fcf6b1865e276434
IceyWu <kui.wu@boran-tech.com>
2022-12-06 15:24:46 +0800

    chore: add useTitle and ico

--- src/pages/links/index.vue
+++ src/pages/links/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useFavicon, useTitle } from '@vueuse/core'
 import CollapseList from './components/CollapseList.vue'
 import { useLinksStore } from '~/store/links'
 interface LinkItem {
@@ -10,6 +11,8 @@ interface LinkItem {
   isCollect?: boolean
   tags?: string[]
 }
+const title = useTitle()
+const icon = useFavicon()
 const linksStore = useLinksStore()
 const linkData = computed(() => linksStore.linkData)
 const jump = (data: LinkItem) => {
@@ -20,40 +23,58 @@ const collect = (data: LinkItem) => {
   const { id } = data
   linksStore.saveCollectLink(id)
 }
+const selectItem = ref({} as LinkItem)
+const clickItem = (data: LinkItem) => {
+  const { name = '' } = data
+  title.value = `Links | ${name}`
+  selectItem.value = data
+}
+onMounted(() => {
+  title.value = 'Links'
+  icon.value = '/favicon.ico'
+  selectItem.value = linkData.value[0]
+})
 </script>
 
 <template>
-  <div w-full flex flex-wrap content-start>
+  <div w-full>
     <div v-if="linkData.length === 0" w-full fcc>
       <EmptyData />
     </div>
-    <div
-      v-for="(item) in linkData"
-      v-else :key="item.id"
-      class="shadow rounded-xl overflow-hidden cursor-pointer h-60 w-50 card-box relative flex justify-center items-center mx-4 my-3"
-      @click="jump(item)"
-    >
-      <div class="absolute h-full w-full bg-cover bg z-0 " :style="{ backgroundImage: `url(${item.cover})` }" />
-      <div text-4xl absolute z-10 color-white px-1>
-        <h1>
-          {{ item.name }}
-        </h1>
-        <h3 v-motion-roll-visible-right :delay="15" class="desc" text-xl mt-3 color-stone-100>
-          {{ item.desc }}
-        </h3>
+    <div v-else w-full fcc h-full>
+      <div flex-1 fcc h-full>
+        {{ selectItem }}
       </div>
-      <div class="btn-group absolute z-10 top-2 left-2">
-        <i
-          :class="item.isCollect ? 'i-carbon-favorite-filled text-red-400 hover:opacity-100 hover:text-red-400' : 'i-carbon-favorite'"
-          class="text-[1.5em] text-white"
-          opacity-100
-          icon-btn
-          @click.stop="collect(item)"
-        />
+      <div flex-1 flex flex-wrap content-start h-full>
+        <div
+          v-for="(item) in linkData"
+          :key="item.id"
+          class="shadow rounded-xl overflow-hidden cursor-pointer h-60 w-50 card-box relative flex justify-center items-center mx-4 my-3"
+          @click="clickItem(item)"
+        >
+          <div class="absolute h-full w-full bg-cover bg z-0 " :style="{ backgroundImage: `url(${item.cover})` }" />
+          <div text-4xl absolute z-10 color-white px-1 f-c-c>
+            <h1>
+              {{ item.name }}
+            </h1>
+            <h3 v-motion-roll-visible-right :delay="15" class="desc" text-xl mt-3 color-stone-100>
+              {{ item.desc }}
+            </h3>
+            <div v-motion-slide-visible-once-top class="text-xl de-btn w-fit mt-1.5 btn" @click.stop="jump(item)">
+              ✈ Go De
+            </div>
+          </div>
+          <div class="btn-group absolute z-10 top-2 left-2">
+            <i
+              :class="item.isCollect ? 'i-carbon-favorite-filled text-red-400 hover:opacity-100 hover:text-red-400' : 'i-carbon-favorite'"
+              class="text-[1.5em] text-white"
+              opacity-100
+              icon-btn
+              @click.stop="collect(item)"
+            />
+          </div>
+        </div>
       </div>
-    </div>
-    <div flex-1>
-      <CollapseList />
     </div>
   </div>
 </template>
@@ -67,7 +88,7 @@ const collect = (data: LinkItem) => {
   &:not(:last-child) {
     // margin-right: 2rem;
   }
-  .desc{
+  .desc,.de-btn{
     transform: 0.5s;
     display: none;
   }
@@ -78,7 +99,9 @@ const collect = (data: LinkItem) => {
     filter: blur(2px);
   }
 
-  &:hover .desc{
+  &:hover .desc,
+  &:hover .de-btn
+  {
     display: block;
   }
 }
